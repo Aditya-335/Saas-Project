0b75cccdbbfd944c9a903080a01db2cf8f6d6364
Sawraj <kdmswaraj17@gmail.com>
2024-12-10 22:40:00 +0530

    auth changes

---
+++ bali/src/app/admin/page.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import UserAccountnav from "@/components/UserAccountSignOut";
+import { authOptions } from "@/lib/auth";
+import { Link } from "lucide-react";
+import { getServerSession } from "next-auth"
+import { signOut } from "next-auth/react";
+
+
+const page = async() => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+  return (
+    <>
+    <div>Welcome to admin {session?.user.username}</div>
+    <div>
+        <UserAccountnav></UserAccountnav>
+    
+    
+    </div>
+    </>
+
+    )}
+
+export default page
